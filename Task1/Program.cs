623bceb729ce933db59cd71884468fd15a287757
Lemanyaqubova <lamanny@code.edu.az>
2022-08-04 17:59:13 +0400

    Homework

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 23, 7, 9 };
+            Console.WriteLine(Min(arr));
+
+        }
+        static int Min(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+               
+        }
+
+    }
+}
